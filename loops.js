a4fced1874706f228d2f3173826f9389e1490a36
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-10 21:15:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,6 +22,8 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
+}
+
     do{ array=array.pop } while (array.length > 0 && maybeTrue());
   
 }
